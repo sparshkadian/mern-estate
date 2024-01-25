9d7f6fc53250b9bb1f2958c3d690fbd6cf91e4b1
Sparsh Kadian <sparshkadian323@gmail.com>
2024-01-25 21:17:00 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;

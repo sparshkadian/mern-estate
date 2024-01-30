be6662cd2090881fa360db7d4451c0bb04984469
Sparsh Kadian <sparshkadian323@gmail.com>
2024-01-30 15:07:59 +0530

    add redux-persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/userSlice';
 
+const rootReducer = combineReducers({ user: userReducer });
+
+const persisConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persisConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
